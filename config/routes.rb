bc2ecd1f911456ce6edf2f07c00087006680fb77
rolandfbk <rolandfbk@gmail.com>
2022-09-05 14:00:57 +0200

    Initial setup and splash screen design done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "splash#index"
+
+  resources :users
+  resources :categories, only: [:index, :show, :new, :create, :update, :destroy] do
+    resources :payments, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  end
 end
