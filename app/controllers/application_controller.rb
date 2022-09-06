1da699fe92e17e8a63c1edf89cfc83c9b2961ab7
rolandfbk <rolandfbk@gmail.com>
2022-09-06 16:12:04 +0200

    Linters added and fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
-  
+
   before_action :require_login
 
   before_action :update_allowed_parameters, if: :devise_controller?
@@ -17,10 +17,6 @@ class ApplicationController < ActionController::Base
   private
 
   def require_login
-    unless current_user
-      if request.url == root_url
-        redirect_to splash_index_path
-      end
-    end
+    redirect_to splash_index_path if !current_user && (request.url == root_url)
   end
 end
